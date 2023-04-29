d59a9b3e2e3e8d415e067f30b6e64db2cc0427e0
jaszhang <jaszhang@cs.washington.edu>
2023-04-28 20:57:31 -0700

    pls god pls

--- set.js
+++ set.js
@@ -128,7 +128,7 @@ function startGame() {
       for (let i = 0; i < cardsToUnselect.length; i++) {
         cardsToUnselect[i].classList.remove('selected');
         cardsToUnselect[i].classList.remove('hide-imgs');
-        card.removeEventListener('click', cardSelected);
+        cardsToUnselect[i].removeEventListener('click', cardSelected);
       }
     }
   }
